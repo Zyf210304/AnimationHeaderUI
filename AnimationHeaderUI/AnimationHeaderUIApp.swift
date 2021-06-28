0c4c850e957dc93ea9635e14d0121d4895552ae0
张亚飞 <672233580@qq.com>
2021-06-28 12:20:49 +0800

    Initial Commit

---
+++ AnimationHeaderUI/AnimationHeaderUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationHeaderUIApp.swift
+//  AnimationHeaderUI
+//
+//  Created by 张亚飞 on 2021/6/28.
+//
+
+import SwiftUI
+
+@main
+struct AnimationHeaderUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
