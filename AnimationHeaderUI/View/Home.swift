0fcf4948c53935c5a949dcb9bd69d7d192a6bccb
张亚飞 <672233580@qq.com>
2021-06-29 12:06:44 +0800

    飞

---
+++ AnimationHeaderUI/View/Home.swift
@@ -0,0 +1,36 @@
+//
+//  Home.swift
+//  AnimationHeaderUI
+//
+//  Created by 张亚飞 on 2021/6/28.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        
+        ZStack(alignment: .top) {
+            
+            
+        }
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
+
+var edges = UIApplication.shared.windows.first?.safeAreaInsets
+
+struct HeaderView: View {
+    
+    var body: some View {
+        
+        HStack {
+            
+        }
+    }
+}
